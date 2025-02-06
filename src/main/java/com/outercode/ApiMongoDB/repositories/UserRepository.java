4b736c52fe2d585db1b2076fba5e492018681915
Vladimir Monteiro Souza de Lima <vladimir.monteiro021@gmail.com>
2025-02-06 19:10:31 -0300

    feat: added findByEmail

--- src/main/java/com/outercode/ApiMongoDB/repositories/UserRepository.java
+++ src/main/java/com/outercode/ApiMongoDB/repositories/UserRepository.java
@@ -3,5 +3,9 @@ package com.outercode.ApiMongoDB.repositories;
 import com.outercode.ApiMongoDB.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
 }
